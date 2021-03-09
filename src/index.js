8e389522d0aa017c29f9fe8845140a00d498ba3c
valdik-g <77793654+valdik-g@users.noreply.github.com>
2021-03-09 18:43:24 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 module.exports = function reverse (n) {
   let m = String(n);
+  if(m[0] == "-"){
+      m.splice(0,1);
+      let k = m.split("").reverse().join("");
+      let j = Number(k);
+      j*=-1;
+  }
   let k = m.split("").reverse().join("");
   let j = Number(k);
   return j;
